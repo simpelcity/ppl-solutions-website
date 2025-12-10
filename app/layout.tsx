1cd7898cbf046c8506d593607e14da3071cc7c59
Wietse Verkade <91530374+simpelcity@users.noreply.github.com>
2025-12-10 20:02:17 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <meta name="google-site-verification" content="duVuERpK5FIEgDN0EYvy3aDZQukmh7XKYXITYPPj-4w" />
         <link
           href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200..1000;1,200..1000&family=Ubuntu+Mono:ital,wght@0,400;0,700;1,400;1,700&display=swap"
           rel="stylesheet"
