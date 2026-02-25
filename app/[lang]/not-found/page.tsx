3fcb5259fa2e3948a6504cc928bcc802f07e5b44
Wietse Verkade <91530374+simpelcity@users.noreply.github.com>
2026-02-25 22:15:37 +0100

    Enhance NotFoundPage with styled 404 message

--- app/[lang]/not-found/page.tsx
+++ app/[lang]/not-found/page.tsx
@@ -1,5 +1,13 @@
+import { Container, Card } from 'react-bootstrap';
+
 function NotFoundPage() {
-	return <h1>Page not found</h1>
+	return (
+		<div className="d-flex flex-column justify-content-center align-items-center bg-dark text-light">
+		    <p>Uh, oh</p>
+			<h1>404</h1>
+			<h3>Page not found</h3>
+		</div>
+	)
 }
 
 export default NotFoundPage
