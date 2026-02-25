d2eb3c63a3e676b9fefa010e94cf8c4b6bd790b2
Wietse Verkade <91530374+simpelcity@users.noreply.github.com>
2026-02-25 22:22:45 +0100

    Refactor NotFoundPage layout structure

--- app/[lang]/not-found/page.tsx
+++ app/[lang]/not-found/page.tsx
@@ -2,11 +2,11 @@ import { Container, Card } from 'react-bootstrap';
 
 function NotFoundPage() {
 	return (
-		<div className="d-flex flex-column justify-content-center align-items-center bg-dark text-light">
+		<main className="main d-flex flex-column justify-content-center align-items-center bg-dark text-light">
 		    <p>Uh, oh</p>
 			<h1>404</h1>
 			<h3>Page not found</h3>
-		</div>
+		</main>
 	)
 }
 
