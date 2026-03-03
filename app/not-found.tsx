26f5a5323896e616bc4c274e1b651d7c6cb15d45
Simpelcity <wietse2007.3@gmail.com>
2026-03-03 12:17:45 +0100

    feat: update NotFoundPage component to include layout providers and improve structure

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -3,6 +3,8 @@ import Card404 from '@/components/ui/cards/Card404'
 import { getDictionary } from '@/app/i18n'
 import { i18n, type Locale } from '@/i18n'
 import { cookies } from "next/headers"
+import { AuthProvider, SidebarProvider } from "@/lib"
+import LayoutClient from "./[lang]/LayoutClient"
 
 export const metadata = {
   title: '404 | Page Not Found',
@@ -18,16 +20,22 @@ export default async function NotFoundPage() {
   const dict = await getDictionary(lang)
 
   return (
-    <main className="main">
-      <section className="d-flex w-100 text-light">
-        <Container className="my-5 d-flex justify-content-center">
-          <Row className="w-100 d-flex justify-content-center align-items-center">
-            <Col xs={12} md={10} xl={6}>
-              <Card404 dict={dict} lang={lang} />
-            </Col>
-          </Row>
-        </Container>
-      </section>
-    </main>
+    <AuthProvider>
+      <SidebarProvider>
+        <LayoutClient dict={dict} lang={lang as Locale} forceHideFooter>
+          <main className="main">
+            <section className="d-flex w-100 text-light">
+              <Container className="my-5 d-flex justify-content-center">
+                <Row className="w-100 d-flex justify-content-center align-items-center">
+                  <Col xs={12} md={10} xl={6}>
+                    <Card404 dict={dict} lang={lang} />
+                  </Col>
+                </Row>
+              </Container>
+            </section>
+          </main>
+        </LayoutClient>
+      </SidebarProvider>
+    </AuthProvider>
   )
 }
