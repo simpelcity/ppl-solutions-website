f1c82e66be9e8734db265d85512b0387fbe27856
Wietse Verkade <91530374+simpelcity@users.noreply.github.com>
2026-03-31 22:12:46 +0200

    Update layout.tsx

--- app/[lang]/layout.tsx
+++ app/[lang]/layout.tsx
@@ -5,12 +5,16 @@ import LayoutClient from "../[lang]/LayoutClient"
 
 type Props = {
   children: React.ReactNode
-  params: Promise<{ lang: Locale }>
+  params: Promise<{ lang: string }>
+}
+
+export async function generateStaticParams() {
+  return i18n.locales.map((locale) => ({ lang: locale }))
 }
 
 export default async function RootLayout({ children, params }: Props) {
   const { lang } = await params
-  const dict = await getDictionary(lang)
+  const dict = await getDictionary(lang as Locale)
 
   return (
     <AuthProvider>
