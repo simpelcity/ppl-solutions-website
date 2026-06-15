40096820d9d5e7fe32fa9b9629a95af6627599bb
Wietse Verkade <91530374+simpelcity@users.noreply.github.com>
2026-06-15 18:49:12 +0200

    added carousel to HomeBanner (#24)

--- app/components/sections/HomeBanner.tsx
+++ app/components/sections/HomeBanner.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { useTheme } from 'next-themes'
-import { Container, Image } from 'react-bootstrap'
+import { Container, Image, Carousel } from 'react-bootstrap'
 import { BSButton } from '@/components'
 import type { Dictionary } from "@/app/i18n"
 import { type Locale } from "@/i18n"
@@ -16,16 +16,30 @@ export default function HomeBanner({ dict, lang }: Props) {
 
   const currentLang = lang === 'en' ? '' : `/${lang}`;
 
+  const carouselItems = [
+    { href: `/assets/images/${resolvedTheme ? resolvedTheme : ''}/home-banner.jpg`, alt: dict.home.homeBannerAlt[resolvedTheme as 'light' | 'dark'] || dict.home.homeBannerAlt.default },
+    { href: '/assets/images/gallery/lukas-scania.jpg', alt: 'Scania by Lukyy09' },
+    { href: '/assets/images/gallery/maleklecocze-save-edit.png', alt: 'Save-edit by MaleklecoCZE' },
+    { href: '/assets/images/gallery/simpelcity-daf.jpg', alt: 'DAF by Simpelcity' },
+    { href: '/assets/images/gallery/simpelcity-maleklecocze-2.jpg', alt: "Scania's by Simpelcity" },
+  ];
+
   return (
     <section className="d-flex w-100 text-light">
       <Container className="px-0 position-relative d-flex justify-content-center" fluid>
-        <Image
-          src={`/assets/images/${resolvedTheme ? resolvedTheme : ''}/home-banner.jpg`}
-          alt={dict.home.homeBannerAlt[resolvedTheme as 'light' | 'dark'] || dict.home.homeBannerAlt.default}
-          className="object-fit-cover w-100 home-img"
-          fluid
-        />
-        <div className=" position-absolute top-0 w-100 h-100 d-flex flex-column justify-content-center align-items-center">
+        <Carousel data-bs-theme="light" controls={false} fade className="w-100">
+          {carouselItems.map((item, index) => (
+            <Carousel.Item key={index}>
+              <Image
+                src={item.href}
+                alt={item.alt}
+                className="object-fit-cover w-100 home-img"
+                fluid
+              />
+            </Carousel.Item>
+          ))}
+        </Carousel>
+        <div className="z-1 position-absolute top-0 w-100 h-100 d-flex flex-column justify-content-center align-items-center">
           <h4 className="text-center">"{dict.home.slogan}"</h4>
           <div className="mt-3 d-flex justify-content-center column-gap-3">
             <BSButton variant="outline" size="lg" border="light" href={`${currentLang}/apply`}>
